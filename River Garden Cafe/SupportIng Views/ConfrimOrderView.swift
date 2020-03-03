a164ef54586d81dfbbf87156e235051c3f61cf7c
Nigel Gee <nigel.gee@live.com>
2020-03-03 11:44:44 +0000

    Commit

---
+++ River Garden Cafe/SupportIng Views/ConfrimOrderView.swift
@@ -0,0 +1,23 @@
+//
+//  ConfrimOrderView.swift
+//  River Garden Cafe
+//
+//  Created by Nigel Gee on 15/02/2020.
+//  Copyright © 2020 Nigel Gee. All rights reserved.
+//
+
+
+import SwiftUI
+
+
+struct ConfrimOrderView: View {
+    var body: some View {
+        Text("File to be deleted")
+    }
+}
+
+struct ConfrimOrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfrimOrderView()
+    }
+}
